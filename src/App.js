c3cc06445d255de3fce86afb83ff3734c96b4c3a
blackhat97 <junwoopark97@naver.com>
2018-08-22 06:26:47 +0000

    init commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import './App.css';
+import LetterOfCredit from './components/LetterOfCredit/LetterOfCredit.js';
+import AlicePage from './components/Pages/AlicePage/AlicePage.js';
+import BobPage from './components/Pages/BobPage/BobPage.js';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentLetter: {},
+      currentUser: "alice"
+    };
+    this.changeUser = this.changeUser.bind(this);
+    this.goToLetterScreen = this.goToLetterScreen.bind(this);
+  }
+
+  goToLetterScreen(letter, isApply) {
+    this.setState({
+      currentLetter: letter,
+      isApply: isApply
+    });
+  }
+
+  changeUser(user) {
+    if(user === 'alice') {
+      this.setState({
+        currentUser: "alice"
+      });
+    } else if (user === 'bob') {
+      this.setState({
+        currentUser: "bob"
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/alice" render={(props) => <AlicePage user={this.state.currentUser} switchUser={this.changeUser} callback={this.goToLetterScreen} {...props}/>}/>
+        <Route exact path="/bob" render={(props) => <BobPage user={this.state.currentUser} switchUser={this.changeUser} callback={this.goToLetterScreen} {...props}/>}/>
+        <Route path="/:name/loc/:id" render={(props) => <LetterOfCredit letter={this.state.currentLetter} callback={this.changeUser} isApply={this.state.isApply} user={this.state.currentUser} {...props}/>}/>
+      </Switch>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/reducers.js
@@ -0,0 +1,63 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {combineReducers} from 'redux'
+import * as actions from '../actions/actions'
+
+const initialState = {
+  charge: {
+    name: 'Alice Hamilton',
+    companyName: 'QuickFix IT',
+    bankName: 'Bank of Dinero',
+    account: '123'
+  },
+  payment: {
+    name: 'Bob Appleton',
+    companyName: 'Conga Computers',
+    bankName: 'Eastwood Banking',
+    account: '122'
+  },
+  productDetails: {
+    type: "None",
+    quantity: 0,
+    pricePerUnit: 0,
+    total: 0,
+    payment_date: "String"
+  },
+  rules: [
+    {ruleText: "The correct quantity of product has been delivered."},
+    {ruleText: "The product was received within 30 days of the placement of the order."},
+    {ruleText: "The product is not damaged and functions as expected."}
+  ]
+}
+
+const getLetterInputReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.GET_CHARGE:
+      return { ...state, charge: action.payload };
+    case actions.GET_PAYMENT:
+      return { ...state, payment: action.payload };
+    case actions.GET_PRODUCT_DETAILS:
+      return { ...state, productDetails: action.payload };
+    case actions.GET_RULES:
+      return { ...state, rules: [...state.rules, action.payload] };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+   getLetterInputReducer
+})
+
+export default rootReducer;
